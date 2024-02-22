884a01cb2353c71987d6399d2f9e4038dc917c2f
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-02-22 08:12:38 +0100

    feat(FE)script. add tinymce. add printReadDoc function

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,29 @@
+tinymce.init({
+    selector: '#editArea',
+    plugins: 'code',
+    toolbar: 'undo redo | forecolor backcolor | styleselect bold italic underline | alignleft aligncenter alignright | code',
+
+    setup: function(editor) {
+        editor.on('change', function() {
+            editor.save();
+        })
+    }
+});
 
 import {storeUser, getUser, logoutUser} from "./modules/user.js";
 
 
-const loginButton = document.getElementById('loginButton');
-const loginDiv = document.getElementById('loginDiv');
-const logoutDiv = document.getElementById('logoutDiv');
+let loginButton = document.getElementById('loginButton');
+let loginDiv = document.getElementById('loginDiv');
+let logoutDiv = document.getElementById('logoutDiv');
+let documentList = document.getElementById('documentList');
+let documentContainer = document.getElementById('documentContainer');
 
 function init() {
-    console.log(getUser());
+    //Is user logged in?
     if (getUser() !== null ) {
         toggleHiddenClass();
+        printDocList();
     } 
 }
 
@@ -31,7 +45,7 @@ loginButton.addEventListener('click', () => {
         body: JSON.stringify({ email: email, password: password })
     })
     .then(res => res.json())
-    .then(data => {
+    .then(async (data) => {
         console.log('detta är data', data);
         //lägga in user i localstorage
         storeUser(data);
@@ -43,6 +57,7 @@ loginButton.addEventListener('click', () => {
         //dölja loginfälten
         toggleHiddenClass();
         //printa userns dokument i en lista
+        await printDocList();
 
     })
 });
@@ -50,10 +65,94 @@ loginButton.addEventListener('click', () => {
 logoutButton.addEventListener('click', () => {
     logoutUser();
     loginDiv.classList.remove('hidden');
+    logoutDiv.classList.add('hidden');
+    documentList.innerHTML = '';
 });
-//funktion för att printa dokument
-//funktion för att visa i edit-mode
+
+async function printDocList() {
+    try {
+        const user = await getUser();
+        const response = await fetch(`http://localhost:3000/documents/?userId=${user.id}`)
+        const data = await response.json();
+
+        //console.log('detta är printdatan:', data.documents);
+        documentList.innerHTML = '';
+        data.documents.map(doc => {
+            let li = document.createElement('li')
+            li.innerText = doc.heading;
+
+            li.addEventListener('click', () => {
+
+                console.log('du klickar på: ', doc.id);
+                //funktion för att öppna doc i läsläge
+                printReadDoc(doc);
+            })
+
+            documentList.appendChild(li);
+        })
+
+
+
+    } catch (error) {
+        console.error('Error fetching list', error);
+        response.status(500).json({message: 'Internal server error', error: error});
+    }
+
+
+}
+
+
 //funktion för att visa i read-mode
+function printReadDoc(doc) {
+   
+    const article = document.createElement('article');
+    article.classList.add('read_document_container');
+
+    const heading = document.createElement('h3');
+    heading.innerText = doc.heading;
+    heading.id = 'readDocumentHeading';
+
+    const created = document.createElement('p');
+    created.innerText = `Created: ${doc.created}`;
+    created.id = 'readCreated';
+
+    const lastEdited = document.createElement('p');
+    lastEdited.innerText = `Last edited: ${doc.lastEdited}`;
+    lastEdited.id = 'readLastEdited';
+
+    const content = document.createElement('div');
+    content.classList.add('read_document');
+    content.id = 'readDocumentContent';
+    content.innerText = doc.content;
+
+    const editButton = document.createElement('button');
+    editButton.id = 'editButton';
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => {
+        console.log('clicketiclack');
+    })
+
+    const closeButton = document.createElement('button');
+    closeButton.id = 'closeButton';
+    closeButton.innerText = 'Close';
+    closeButton.addEventListener('click', () => {
+        documentContainer.innerHTML = ''; 
+    })
+
+
+    // Lägg till de skapade elementen i article-elementet
+    article.appendChild(heading);
+    article.appendChild(created);
+    article.appendChild(lastEdited);
+    article.appendChild(content);
+    article.appendChild(editButton);
+    article.appendChild(closeButton);
+
+    documentContainer.innerHTML = ''; 
+    documentContainer.appendChild(article);
+}
+
+//funktion för att visa i edit-mode
 //spara editerat dokument i databas
 //Skapa nytt dokument, 2 fields ett för heading ett för content
 //spara dokument i databas
