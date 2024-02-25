bfd04a7fceab49e2c114a7252feb258c6109b8b4
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-02-25 19:13:19 +0100

    add databse dump

---
+++ backend/database/QuickNotesUsers.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM quick_notes.users
+LIMIT 0, 1000
+
+-- Date: 2024-02-25 19:08
+*/
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (1,'Anny Nysson','anny@mail.com','test','2024-02-16 14:03:43');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (2,'Bosse Nysson','bo@mail.com','test','2024-02-16 14:07:07');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (3,'Isak Tonåring','isak@mail.com','test','2024-02-16 14:14:26');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (4,'Leo Lallo','leo@mail.com','test','2024-02-16 14:14:26');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (5,'Janne K','janne@mail.com','test','2024-02-16 14:14:26');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (6,'Tobbe Tobbsson','tobbe@mail.com','test','2024-02-16 14:14:26');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (7,'Bailey Bailanson','bailey@mail.com','test','2024-02-16 14:14:26');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (8,'Masken Maskansson','masken@mail.com','test','2024-02-20 11:23:47');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (9,'Tråk Tråkesson','trak@mail.com','test','2024-02-22 18:45:28');
+INSERT INTO `` (`id`,`name`,`email`,`password`,`created`) VALUES (10,'Mans Mansson','mans@mail.com','test','2024-02-22 18:57:23');
